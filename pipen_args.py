4b7f0262429d4d6466a6accd9e89af7ed4a8699b
pwwang <pwwang@pwwang.com>
2021-11-12 16:44:55 -0700

    🔖 0.1.5

--- pipen_args.py
+++ pipen_args.py
@@ -12,7 +12,7 @@ from pipen.utils import _logger_handler, copy_dict
 from pyparam import Params, defaults
 from simpleconf import Config
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 # Allow type to be overriden from command line
 defaults.PARAM.type_frozen = False
