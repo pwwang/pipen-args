ba6af8729295bfea3e80636132f9931e59fc21d0
pwwang <pwwang@pwwang.com>
2022-09-23 22:54:26 -0700

    📝 Add example for config

---
+++ examples/example-config.py
@@ -0,0 +1,16 @@
+from pipen import Proc, Pipen
+from pipen_args import args, config
+
+# Get the config from "--config" file even before parsed
+print(config)
+
+
+class Process(Proc):
+    """My process"""
+    input = 'a'
+    output = 'b:file:b.txt'
+    input_data = range(10)
+    script = 'echo {{in.a}} > {{out.b}}'
+    plugin_opts = {"report": "a"}
+
+Pipen(desc='Pipeline description.').set_start(Process).run()
