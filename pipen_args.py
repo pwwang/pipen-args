02ed6fababe9efbdee62692f92d0c0fbb2b1960a
pwwang <pwwang@pwwang.com>
2023-01-26 08:50:40 -0700

    🔖 0.3.5

--- pipen_args.py
+++ pipen_args.py
@@ -16,7 +16,7 @@ from simpleconf import ProfileConfig, Config
 
 from slugify import slugify
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 # Allow type to be overriden from command line
 defaults.PARAM.type_frozen = False
